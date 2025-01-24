683426f5ecb3bd8c89002952637544920a00feb8
JOSIAS VALENTIM DE FIGUEREDO <josiasvfigueredo@gmail.com>
2025-01-24 10:55:41 -0300

    Webpack configs updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/assets/typescript/app.ts', // Arquivo de entrada
@@ -20,22 +21,39 @@ module.exports = {
         test: /\.ts$/, // Aplica o loader em arquivos .ts
         use: 'ts-loader', // Utiliza o ts-loader para transpilar TypeScript e MiniCssExtractPlugin.loader, css-loader para CSS
         exclude: /node_modules/
-      }
+      },{
+            test: /\.css$/, // Aplica o css-loader e MiniCssExtractPlugin apenas em arquivos .css
+        use: [
+              MiniCssExtractPlugin.loader,
+                'css-loader',
+            ],
+            exclude: /node_modules/,
+        },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html', // Gera um HTML com o bundle
     }),
-      // new MiniCssExtractPlugin({
-      //       filename: 'scr/assets/css/style.css', // Define onde o CSS será salvo
-      //   }),
+      new MiniCssExtractPlugin({
+        filename: 'assets/css/style.css', // Define onde o CSS será salvo
+      }),
+      new CopyWebpackPlugin({
+      patterns: [
+          { from: 'src/assets/css', to: 'assets/css' },
+          { from: 'src/assets/fonts', to: 'assets/fonts' },
+          { from: 'src/assets/images', to: 'assets/images' },
+          { from: 'src/assets/scss', to: 'assets/scss' },
+          { from: 'src/assets/sounds', to: 'assets/sounds' },
+      ],
+    }),
   ],
   devServer: {
     static: './dist', // Servir arquivos da pasta dist
     port: 3000, // Porta do servidor
     open: true, // Abre o navegador automaticamente
-   // compress: true
+    historyApiFallback: true, 
+    compress: true
   },
   mode: 'development' // Modo de desenvolvimento
 };
