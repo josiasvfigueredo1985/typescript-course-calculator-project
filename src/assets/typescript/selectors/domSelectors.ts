339563392d34e22d0d01600fa925b96e3835f415
JOSIAS VALENTIM DE FIGUEREDO <josiasvfigueredo@gmail.com>
2025-01-28 10:09:07 -0300

    Selectors update

--- src/assets/typescript/selectors/domSelectors.ts
+++ src/assets/typescript/selectors/domSelectors.ts
@@ -1,14 +1,16 @@
 const calculadora = '#calculadora'
 const tela = '#tela'
 const display = '#display'
-const dateTime = '#datetime'
+const topDisplay = '#top-display'
+const date = '#date'
+const time = '#time'
 const teclado = '#teclado'
 const acoes = '#acoes'
 const settingsInfo = '#display-settings'
 
 export const displaySelectors = {
-    dateEl: `${calculadora} ${tela} ${display} ${dateTime}>div:nth-child(2)`,
-    timeEl: `${calculadora} ${tela} ${display} ${dateTime} time`,
+    dateEl: `${calculadora} ${tela} ${display} ${topDisplay}  ${date}`,
+    timeEl: `${calculadora} ${tela} ${display} ${time} time`,
     settingsEl: `${calculadora} ${tela} ${display} ${settingsInfo}`,
     displayValuesEl: `${calculadora} ${tela} ${display} #values`,
     soundIconEl: `${calculadora} ${tela} ${display} ${settingsInfo} #sound-icon`,
